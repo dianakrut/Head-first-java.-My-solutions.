043120125d6382474cffa0c8af4fd6935056f883
Di <medianarider@gmail.com>
2021-01-05 13:29:47 +0300

    remove duplication

--- src/Exerciselb.java
+++ src/Exerciselb.java
@@ -1,19 +1,5 @@
-// A
-public class Exerciselb {
-    public static void main(String[] args) {
-        int x = 5;
-        while (x > 1) {
-            x = x - 1;
-            if (x < 3) {
-                System.out.println("маленький икс");
-            }
-        }
-
-    }
-}
-
 //A:
-class ExerciselbA {
+public class Exerciselb {
     public static void main(String[] args) {
         int x = 1;
         while (x < 10) {
@@ -22,7 +8,6 @@ class ExerciselbA {
                 System.out.println("большой икс");
             }
         }
-
     }
 }
 
